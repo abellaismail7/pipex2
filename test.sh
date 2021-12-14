afff5ee410b9d11c849bbc3183ecb836235655a5
Ismail Ait bella <iait-bel@e2r8p3.1337.ma>
2021-12-14 14:21:18 +0100

    my fav tester

---
+++ test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+shtest="< $1 "
+pipetest="./pipex $1"
+
+i=0;
+for cmd in "$@" 
+do
+	if [[ $i -ne 0 ]]
+	then
+		shtest+="$cmd "
+		pipetest+=" '$cmd'"
+		if [[ $i -ne $(($# - 1)) ]]
+		then
+			shtest+="| "
+		fi
+	fi
+    i=$((i + 1));
+done
+
+
+echo ">>> $shtest > realout"
+bash -c "$shtest > realout"; tput setaf 2; echo " ($?)"; tput sgr0;
+
+echo ">>> $pipetest urout"
+bash -c "$pipetest urout"; tput setaf 2; echo " ($?)"; tput sgr0;
+
+diff realout urout
